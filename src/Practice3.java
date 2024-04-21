b410062fb2d34a0c4cbcf691c8af148b8bf10721
김인태 <114kit123@gmail.com>
2024-04-21 16:23:33 +0900

    편집기 구현

--- src/Practice3.java
+++ src/Practice3.java
@@ -1,7 +1,54 @@
+// 편집기 구현
+
+/*
+아이디어:
+1. StringBuffer 사용 (문자열의 변동이 많기 때문)
+2. 커서의 인덱스 변수 지정
+3. Math.max or min 으로 index 범위 조절
+ */
+
+import java.util.ArrayList;
 
 public class Practice3 {
     public static String solution(String input, String cmd) {
-        return null;
+        StringBuffer sb = new StringBuffer(input);
+        ArrayList<String> cmdArr = new ArrayList<>();
+
+        for (String s : cmd.split(" ")) {
+            cmdArr.add(s);
+        }
+
+
+        int curSor = sb.length();
+        int cmdIdx = 0;
+
+        while (cmdIdx != cmdArr.size()) {
+            String cur = cmdArr.get(cmdIdx);
+
+            if (cur.equals("L")) {
+                curSor = Math.max(0, curSor - 1);
+            } else if (cur.equals("D")) {
+                curSor = Math.min(sb.length(), curSor + 1);
+            } else if (cur.equals("B")) {
+                if (curSor == 0) {
+                    cmdIdx++;
+                    continue;
+                }
+
+                sb.delete(curSor -1 , curSor);
+                curSor = Math.max(0, curSor - 1);
+
+            } else if (cur.equals("P")) {
+                String s = cmdArr.get(++cmdIdx);
+                sb.insert(curSor, s);
+                curSor++;
+            }
+            cmdIdx++;
+        }
+
+
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
