def45127ea6a7f24f8411e51921cfc290a54e041
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2024-04-25 14:41:53 +0900

    자바 연습 문제 3
    자바 연습 문제 3

---
+++ Java_18_3/src/Practice1.java
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+//로마 숫자 표기
+
+import java.util.HashMap;
+
+public class Practice1 {
+    public static void solution(String s) {
+        HashMap<Character , Integer> map = new HashMap<>();
+        map.put('I' , 1);
+        map.put('V' , 5);
+        map.put('X' , 10);
+        map.put('L' , 50);
+        map.put('C' , 100);
+        map.put('D' , 500);
+        map.put('M' , 1000);
+
+        int sum = 0;
+        char[] arr = s.toCharArray(); // String s로 넘어온 문자열을 하나하나 배열로 넣기
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (map.get(arr[i]) < map.get(arr[i + 1])) {
+                sum -= map.get(arr[i]);
+            } else {
+                sum += map.get(arr[i]);
+            }
+        }
+        sum += map.get(arr[arr.length - 1]);
+        System.out.println(sum);
+
+=======
+
+public class Practice1 {
+    public static void solution(int[] nums) {
+>>>>>>> 14c122c (자바 연습문제 3)
+
+    }
+
+    public static void main(String[] args) {
+        // Test code
+<<<<<<< HEAD
+        solution("III");
+        solution("IV");
+        solution("VI");
+        solution("XIII");
+        solution("XXVI");
+        solution("MCMXCIV");
+=======
+        solution(new int[] {1, 1, 2});
+        solution(new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4});
+>>>>>>> 14c122c (자바 연습문제 3)
+    }
+}
---
+++ Java_18_3/src/Practice3.java
@@ -0,0 +1,83 @@
+<<<<<<< HEAD
+// 편집기 구현
+
+/*
+아이디어:
+1. StringBuffer 사용 (문자열의 변동이 많기 때문)
+2. 커서의 인덱스 변수 지정
+3. Math.max or min 으로 index 범위 조절
+ */
+
+import java.util.ArrayList;
+
+public class Practice3 {
+    public static String solution(String input, String cmd) {
+        StringBuffer sb = new StringBuffer(input);
+        ArrayList<String> cmdArr = new ArrayList<>();
+
+        for (String s : cmd.split(" ")) {
+            cmdArr.add(s);
+        }
+
+
+        int curSor = sb.length();
+        int cmdIdx = 0;
+
+        while (cmdIdx != cmdArr.size()) {
+            String cur = cmdArr.get(cmdIdx);
+
+            if (cur.equals("L")) {
+                curSor = Math.max(0, curSor - 1);
+            } else if (cur.equals("D")) {
+                curSor = Math.min(sb.length(), curSor + 1);
+            } else if (cur.equals("B")) {
+                if (curSor == 0) {
+                    cmdIdx++;
+                    continue;
+                }
+
+                sb.delete(curSor -1 , curSor);
+                curSor = Math.max(0, curSor - 1);
+
+            } else if (cur.equals("P")) {
+                String s = cmdArr.get(++cmdIdx);
+                sb.insert(curSor, s);
+                curSor++;
+            }
+            cmdIdx++;
+        }
+
+
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        // test code
+        System.out.println(solution("aba", "L B"));
+        System.out.println(solution("abcd", "P x L P y"));
+        System.out.println(solution("abc", "L L L P x L B P y"));
+        System.out.println(solution("a", "B B L L D D P a P b P c"));
+=======
+
+public class Practice3 {
+    public static void solution(int[] arr, int k, int x) {
+
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        int[] arr = {1, 2, 3, 4, 5};
+        solution(arr, 4, 3);
+
+        arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        solution(arr, 5, 5);
+
+        arr = new int[]{2, 4};
+        solution(arr, 1, 3);
+
+        arr = new int[]{2, 4};
+        solution(arr, 3, 3);
+>>>>>>> 14c122c (자바 연습문제 3)
+    }
+}
